741f185cf2dc2400e0e84942093c494d2aedd228
Bo01la <abdoehab30@gmail.com>
2025-07-10 00:27:51 +0300

    same functionality but this time by using "redux toolkit"

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,26 +1,34 @@
 import { useSelector, useDispatch } from "react-redux";
 
 import classes from "./Counter.module.css";
+import { counterSliceActions } from "../store/store"; // imported our slice actions
 
 const Counter = () => {
   const counter = useSelector((state) => state.counter);
-  const show = useSelector((state) => state.showCounter); // extracting the new state
-
+  const show = useSelector((state) => state.showCounter);
   const dispatch = useDispatch();
 
   const incrementHandler = () => {
-    dispatch({ type: "increment" });
+    // using the methods we created in slice in the store
+    dispatch(counterSliceActions.increase());
   };
   const decrementHandler = () => {
-    dispatch({ type: "decrement" });
+    dispatch(counterSliceActions.decrease);
   };
 
   const addFiveHandler = () => {
-    dispatch({ type: "add-5", amount: 5 });
+    // here we pass the value (number, string, object, array, etc) to the function
+    // as a payload. Thus, when we access it in the "Slice methods" we use ".payload"
+    // because when redux toolkit creates an action for us, it creates it as an object with these
+    // default properties {type: "a uniue identifier, payload: (values we add of any type)"}
+    dispatch(counterSliceActions.addFive(5));
+
+    // it can also be like this if we have multiple values to add:
+    // dispatch(counter.addFive({value1: 1, value2: 2, value3: 3}))
   };
-// new dispatch function to trigger action manipulating the new state
+
   const toggleCounterHandler = () => {
-    dispatch({ type: "toggle" });
+    dispatch(counterSliceActions.showCounter());
   };
 
   return (
--- src/store/store.js
+++ src/store/store.js
@@ -1,38 +1,64 @@
-import { createStore } from "redux";
+// importing "createslice()" to split our state into multiple slices
+// importing "configureStore" so that if we have multiple slices each has its own
+// reducers, we can pass them all to the store [cause the store wants just one big reducer]
 
-const INITIAL_STATE = { counter: 0, showCounter: true }; // adding new state to state object
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-const counterReducer = (state = INITIAL_STATE, action) => {
-  // we have to add the new state to each action because Redux will not merge the
-  // old one with the new one for us
-  if (action.type === "increment") {
-    return { counter: state.counter + 1, showCounter: state.showCounter };
-  }
-  // we have to add the new state to each action because Redux will not merge the
-  // old one with the new one for us
+const INITIAL_STATE = { counter: 0, showCounter: true };
 
-  if (action.type === "add-5") {
-    return {
-      counter: state.counter + action.amount,
-      showCounter: state.showCounter,
-    };
-  }
-  // we have to add the new state to each action because Redux will not merge the
-  // old one with the new one for us
+// NOTE: we can make more than 1 slice as needed (ex: counter slice, user slice, .. etc)
+// we create a slice that include all states related to each other using
+// createSlice() which takes an object as an argumnt with the below properties:
+// 1- name: the name of the slice (can be any name)
+// 2-initialState: which include the initial values of the state slice (same as we did in normal redux)
+// 3- reduces: an object (map) with dispatch function in form of methods
 
-  if (action.type === "decrement") {
-    return { counter: state.counter - 1, showCounter: state.showCounter };
-  }
-  
+// then we pass the returned value of "createSlice()" to our store created by "createStore()"
 
-  // adding the new state but inverted it's value using the (!)
-  if (action.type === "toggle") {
-    return { showCounter: !state.showCounter, counter: state.counter };
-  }
+const counterSlice = createSlice({
+  // 1- name:the name of the slice (can be any name)
+  name: "counterState",
+  // 2-initialState: which include the initial values of the state slice (same as we did in normal redux)
+  initialState: INITIAL_STATE,
+  // 3- reduces: an object (map) with dispatch function in form of methods
+  reducers: {
+    increase(state) {
+      // can mutate the state directly because reduxToolkit creates a copy under the hood
+      state.counter++;
+    },
+    decrease(state) {
+      state.counter--;
+    },
+    // we can also use action argument when needed same as we did in the normal reducer function
+    addFive(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    showCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
 
-  return state;
-};
+//passing the returned value of "createSlice()" to our store created by "configureStore()"
+// configureStore() takes an object as an argument and that object must have
+// "reducer" property
+// if we have one slice, then we can directly add the slice.reducer to the "reducer property"
+// if we have more than 1 slice, we can add an object with (key: value) pairs of each slice and
+// its reducers to the "reducer property"
+const store = configureStore({
+  reducer: counterSlice.reducer,
 
-const store = createStore(counterReducer);
+  // if multiple slices it will be like this:
+  //  reducer: {
+  //   counter: counterSlice.reducer,
+  //   users: usersSlice.reducer,
+  //   someSlice: someSlice.reducer
+  // }
+});
 
+// redux toolkit creates a propert for us called "actions" which includes all
+// the method we created for our slice
+// in order to dispatch actions, we need to export the "sliceName.actions" to use it
+// later in our components
+export const counterSliceActions = counterSlice.actions;
 export default store;
