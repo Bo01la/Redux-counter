8e0c60423bd71b16d8a118da5cc03014c98de1a9
Bo01la <abdoehab30@gmail.com>
2025-07-10 01:56:52 +0300

    handled duplicate (UserProfile) import in App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { useSelector } from "react-redux";
 
 import Header from "./components/Header";
 import Auth from "./components/Auth";
-import UserProfile from "./components/UserProfile";
 import Counter from "./components/Counter";
 import UserProfile from "./components/UserProfile";
 
