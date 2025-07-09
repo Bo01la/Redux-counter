79be0e546176c3279fe7ffeb54278f619f7e276f
Bo01la <abdoehab30@gmail.com>
2025-07-09 19:37:36 +0300

    final code before starting using "redux tolkit"

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,33 +4,32 @@ import classes from "./Counter.module.css";
 
 const Counter = () => {
   const counter = useSelector((state) => state.counter);
-  //useDispatch() hook returns a function that we can call later to
-  // dispatch actions to the reducer function
+  const show = useSelector((state) => state.showCounter); // extracting the new state
+
   const dispatch = useDispatch();
 
   const incrementHandler = () => {
-    // using the above dispatch
-    // it accept an array with a (type) property with value = identifiers in the
-    // reducer function
-
     dispatch({ type: "increment" });
   };
   const decrementHandler = () => {
-    // using the above dispatch
-    // it accept an array with a (type) property with value = identifiers in the
-    // reducer function
-
     dispatch({ type: "decrement" });
   };
 
-  const toggleCounterHandler = () => {};
+  const addFiveHandler = () => {
+    dispatch({ type: "add-5", amount: 5 });
+  };
+// new dispatch function to trigger action manipulating the new state
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>+1</button>
+        <button onClick={addFiveHandler}>+5</button>
         <button onClick={decrementHandler}>-1</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,33 @@
 import { createStore } from "redux";
 
-const INITIAL_STATE = { counter: 0 };
+const INITIAL_STATE = { counter: 0, showCounter: true }; // adding new state to state object
 
 const counterReducer = (state = INITIAL_STATE, action) => {
+  // we have to add the new state to each action because Redux will not merge the
+  // old one with the new one for us
   if (action.type === "increment") {
-    return { counter: state.counter + 1 };
+    return { counter: state.counter + 1, showCounter: state.showCounter };
   }
+  // we have to add the new state to each action because Redux will not merge the
+  // old one with the new one for us
+
+  if (action.type === "add-5") {
+    return {
+      counter: state.counter + action.amount,
+      showCounter: state.showCounter,
+    };
+  }
+  // we have to add the new state to each action because Redux will not merge the
+  // old one with the new one for us
 
   if (action.type === "decrement") {
-    return { counter: state.counter - 1 };
+    return { counter: state.counter - 1, showCounter: state.showCounter };
+  }
+  
+
+  // adding the new state but inverted it's value using the (!)
+  if (action.type === "toggle") {
+    return { showCounter: !state.showCounter, counter: state.counter };
   }
 
   return state;
@@ -16,4 +35,4 @@ const counterReducer = (state = INITIAL_STATE, action) => {
 
 const store = createStore(counterReducer);
 
-export default store; // exporting the store to be used later in the app
+export default store;
